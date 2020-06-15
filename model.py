2166655ec18e12feb75f606780040b3d0a4731a2
Sreehari Menon <braghunathan@trl.co.uk>
2020-06-15 08:36:15 +0100

    low model

--- model.py
+++ model.py
@@ -46,7 +46,7 @@ class Agent():
         self.replace_target = 100
 
         self.Q_eval = DeepQNetwork(lr, n_actions=n_actions, input_dims=input_dims,
-                                    fc1_dims=256, fc2_dims=256, fc3_dims=256)
+                                    fc1_dims=64, fc2_dims=64, fc3_dims=64)
         # self.Q_next = DeepQNetwork(lr, n_actions=n_actions, input_dims=input_dims,
         #                             fc1_dims=64, fc2_dims=64, fc3_dims=64)
 
