e93e3b281765f472d69f10de0de29676ad8517eb
Sreehari Menon <braghunathan@trl.co.uk>
2020-06-24 21:30:49 +0100

    128 batch eps dec increased to 6

--- src/learn_batch/main.py
+++ src/learn_batch/main.py
@@ -6,10 +6,10 @@ import torch as T
 
 if __name__ == '__main__':
     env = gym.make('LunarLander-v2')
-    agent = Agent(gamma=0.99, epsilon=1.0, batch_size=256, n_actions=4, eps_end=0.01,
+    agent = Agent(gamma=0.99, epsilon=1.0, batch_size=128, n_actions=4, eps_end=0.01,
                   input_dims=[8], lr=0.001)
     scores, eps_history = [], []
-    n_games = 4000
+    n_games = 8000
     
     for i in range(n_games):
         score = 0
